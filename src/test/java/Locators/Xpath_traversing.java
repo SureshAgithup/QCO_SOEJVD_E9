adee72f2547a001b0449d8eba677bd409121ca8a
Sures <Sures@ANANYA>
2023-10-13 21:01:38 +0530

    Xpath_E9

--- src/test/java/Locators/Xpath_traversing.java
+++ src/test/java/Locators/Xpath_traversing.java
@@ -2,6 +2,7 @@ package Locators;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -19,7 +20,9 @@ public class Xpath_traversing {
 		
 		//to open the browser
 		driver.get("https://www.amazon.in/Redmi-Storage-Performance-Mediatek-Display/dp/B0BYN4D512/ref=sr_1_1_sspa?crid=9WYJ098K356P&keywords=mobile&qid=1695476096&sprefix=%2Caps%2C250&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1");
-		driver.findElement(By.xpath(""))
+	WebElement price =driver.findElement(By.xpath("(//span[text()='        Redmi 12C (Matte Black, 4GB RAM, 64GB Storage) | High Performance Mediatek Helio G85 | Big 17cm(6.71) HD+ Display with 5000mAh(typ) Battery       ']/../../../../../../..//span[@class='a-price-whole'])[5]"));
+		System.out.println(price.getText());
 	}
+	
 
 }
